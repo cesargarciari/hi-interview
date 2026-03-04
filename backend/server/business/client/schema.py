71e3ab91e9c35e4ed10bb95fdfb071514d1cadf5
cesargarciari <cesarolandogr@gmail.com>
2026-03-03 17:41:44 -0700

    feat(schema): note schema added

--- backend/server/business/client/schema.py
+++ backend/server/business/client/schema.py
@@ -11,3 +11,16 @@ class PClient(BaseModel):
     assigned_user_id: str | None
     created_at: datetime
     updated_at: datetime
+
+
+class PNote(BaseModel):
+    id: str
+    content: str
+    client_id: str
+    created_by_id: str
+    created_by_email: str
+    created_at: datetime
+
+
+class PCreateNoteRequest(BaseModel):
+    content: str
